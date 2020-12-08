b180c9a33b14efbb9088c60bebd29aa64d1b4e38
John Peter <jay3pee@gmail.com>
2020-12-08 16:51:38 +0100

    adjusted the header styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,34 +3,38 @@ import "../assets/cart.svg";
 import { Link } from "react-router-dom";
 export default function Header(props) {
 	return (
-		<div>
-			<header className="header py-3">
-				<div className="container grid">
-					<Link to="/" className="link">
-						<h1 className="brand">ShoppY!</h1>
-					</Link>
-					<form className="search">
-						<div className="form-control">
-							<input
-								type="text"
-								placeholder="Search Items..."
-								onChange={props.search}
-							/>
+		<div className="wrapper">
+			<header className="container">
+				<div className="header py-2">
+					<div className="grid">
+						<Link to="/" className="link">
+							<h1 className="brand">ShoppY!</h1>
+						</Link>
+						<div className="formContainer">
+							<form className="search">
+								<div className="form-control">
+									<input
+										type="text"
+										placeholder="Search Items..."
+										onChange={props.search}
+									/>
+								</div>
+							</form>
 						</div>
-					</form>
-					<Link
-						to={props.cart.length < 1 ? "/" : "/cart"}
-						className="headerCart"
-					>
-						<img className="cartImg" src="cart.svg" alt="" />
-						{props.cart.length > 0 ? (
-							<div className="cartCounter">
-								{props.cart.length <= 9 ? props.cart.length : 9 + "+"}
-							</div>
-						) : (
-							""
-						)}
-					</Link>
+						<Link
+							to={props.cart.length < 1 ? "/" : "/cart"}
+							className="link headerCart"
+						>
+							<img className="cartImg" src="cart.svg" alt="" />
+							{props.cart.length > 0 ? (
+								<div className="cartCounter">
+									{props.cart.length <= 9 ? props.cart.length : 9 + "+"}
+								</div>
+							) : (
+								""
+							)}
+						</Link>
+					</div>
 				</div>
 			</header>
 		</div>
